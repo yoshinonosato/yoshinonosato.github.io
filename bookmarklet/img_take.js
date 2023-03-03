d872e6af9d76d949afdba970661f3727479d647c
yoshinonosato <yoshinonosato@gmail.com>
2023-03-03 11:26:38 +0900

    dmm_openerを追加

--- bookmarklet/img_take.js
+++ bookmarklet/img_take.js
@@ -1,58 +1,42 @@
-// Get a list of all images on the page
-var images = document.querySelectorAll('img');
-
-// Create an empty array to store the unique images
+var images = document.querySelectorAll("img");
 var uniqueImages = [];
-
-// Iterate over the list of images
 for (var i = 0; i < images.length; i++) {
-  // Check if the current image is in the uniqueImages array
-  if (!uniqueImages.includes(images[i].src)) {
-    // If it's not, add it to the array
-    uniqueImages.push(images[i].src);
-  }
+  // if (images[i].src.indexOf(".svg")===-1) {
+    if (!uniqueImages.includes(images[i].src)) {
+      uniqueImages.push(images[i].src);
+    }
+  // }
 }
-
-// Clear the page content
-document.body.innerHTML = '';
-
-// Create a table element
-var table = document.createElement('table');
-
-// Create a row to hold the images
-var row = document.createElement('tr');
-
-// Iterate over the uniqueImages array
+document.body.innerHTML = "";
+var table = document.createElement("table");
+var row = document.createElement("tr");
 for (var i = 0; i < uniqueImages.length; i++) {
-  // Create a cell for the image
-  var imgCell = document.createElement('td');
-  
-  // Set the border, width, and height of the cell
-  imgCell.style.border = '1px solid';
-  imgCell.style.width = '250px';
-  imgCell.style.height = '250px';
-  
-  // Create an img element
-  var img = document.createElement('img');
-  // Set the src attribute to the current image
+
+  console.log(uniqueImages[i]);
+
+  var imgCell = document.createElement("td");
+  imgCell.style.border = "1px solid";
+  imgCell.style.width = "100px";
+  imgCell.style.height = "100px";
+
+  var img = document.createElement("img");
   img.src = uniqueImages[i];
-  // Add the img element to the cell
-  imgCell.appendChild(img);
-  
-  // Add the cell to the row
+  img.style.width = "100%";
+
+  var link = document.createElement("a");
+  link.appendChild(img);
+  link.href=uniqueImages[i];
+  link.target="_blank";
+
+  imgCell.appendChild(link);
   row.appendChild(imgCell);
-  
-  // If the current index is a multiple of 5, add the row to the table and create a new row
-  if (i % 5 === 4) {
+
+  if (i % 20 === 19) {
     table.appendChild(row);
-    row = document.createElement('tr');
+    row = document.createElement("tr");
   }
 }
-
-// Add the final row to the table (if there are any remaining images)
 if (row.children.length > 0) {
   table.appendChild(row);
 }
-
-// Add the table to the page
 document.body.appendChild(table);
