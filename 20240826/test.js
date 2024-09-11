c2818d7f50f897510d6bcc226c68eb1c04b3fea9
yoshinonosato <yoshinonosato@gmail.com>
2024-09-11 11:38:57 +0900

    習作

--- 20240826/test.js
+++ 20240826/test.js
@@ -5,4 +5,41 @@ if (ref) {
 	document.getElementById("text_test").innerHTML = `no ref`;
 }
 
-document.getElementById("text_url").innerHTML = location.search;
+var getUrl = new URL(location.href);
+
+// Discordにメッセージを送信する関数
+function sendMessageToDiscord(webhookUrl, message) {
+	// fetch APIを使用してPOSTリクエストを送信する
+	fetch(webhookUrl, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(message),
+	})
+		.then((response) => response.text())
+		.then((data) => console.log(data))
+		.catch((error) => {
+			console.error("Error:", error);
+		});
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	// ボタン処理
+	const myButton = document.getElementById("send");
+	myButton.addEventListener("click", function () {
+		// メッセージ内容を定義する
+		const messageContent = {
+			content: "button send test",
+		};
+		// メインの処理
+		const discordWebhookUrl = getUrl.searchParams.get("discordWebhookUrl");
+		if (discordWebhookUrl) {
+			// メッセージを送信
+			sendMessageToDiscord(discordWebhookUrl, messageContent);
+			console.log("Discord Msg send.");
+		} else {
+			console.error("DISCORD_WEBHOOK_URL environment variable not set.");
+		}
+	});
+});
